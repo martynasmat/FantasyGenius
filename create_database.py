6e7a82395ed630c923f32c9857827c8d679793c6
martynasmat <martynasmat03@gmail.com>
2025-11-28 23:08:26 +0200

    feat: update teams

---
+++ create_database.py
@@ -0,0 +1,62 @@
+import sqlite3 as s
+
+con = s.connect('database.db')
+cur = con.cursor()
+
+cur.execute("PRAGMA foreign_keys = ON;")
+
+# Teams table
+cur.execute("""
+CREATE TABLE IF NOT EXISTS Teams (
+    team_id      INTEGER PRIMARY KEY,
+    team_name    TEXT NOT NULL UNIQUE,
+    abbreviation TEXT NOT NULL UNIQUE
+);
+""")
+
+# Players table
+cur.execute("""
+CREATE TABLE IF NOT EXISTS Players (
+    player_id     INTEGER PRIMARY KEY,
+    player_name   TEXT NOT NULL,
+    team_id       INTEGER NOT NULL,
+    position      TEXT NOT NULL,
+    fantasy_price REAL NOT NULL,
+    FOREIGN KEY (team_id) REFERENCES Teams(team_id)
+);
+""")
+
+# Games table
+cur.execute("""
+CREATE TABLE IF NOT EXISTS Games (
+    game_id    INTEGER PRIMARY KEY,
+    game_date  TEXT NOT NULL,             -- or DATE; SQLite stores as TEXT internally
+    home_team  INTEGER NOT NULL,
+    away_team  INTEGER NOT NULL,
+    home_score INTEGER NOT NULL,
+    away_score INTEGER NOT NULL,
+    FOREIGN KEY (home_team) REFERENCES Teams(team_id),
+    FOREIGN KEY (away_team) REFERENCES Teams(team_id)
+);
+""")
+
+# Boxscore table
+cur.execute("""
+CREATE TABLE IF NOT EXISTS Boxscore (
+    game_id        INTEGER NOT NULL,
+    player_id      INTEGER NOT NULL,
+    minutes_played REAL NOT NULL,
+    pts            INTEGER NOT NULL,
+    reb            INTEGER NOT NULL,
+    ast            INTEGER NOT NULL,
+    stl            INTEGER NOT NULL,
+    blk            INTEGER NOT NULL,
+    eff            INTEGER NOT NULL,
+    PRIMARY KEY (game_id, player_id),
+    FOREIGN KEY (game_id) REFERENCES Games(game_id),
+    FOREIGN KEY (player_id) REFERENCES Players(player_id)
+);
+""")
+
+con.commit()
+con.close()
---
+++ update_data.py
@@ -0,0 +1,60 @@
+import requests as r
+import sqlite3
+
+DB_PATH = "database.db"
+
+resp = r.get('https://feeds.incrowdsports.com/provider/euroleague-feeds/v2/competitions/E/seasons/E2025/clubs')
+clubs = []
+
+
+def get_teams() -> list[tuple[str, str]]:
+    for club in resp.json()['data']:
+        match club['tvCode']:
+            case 'FBB':
+                abbreviation = 'FEN'
+            case 'KBA':
+                abbreviation = 'BKN'
+            case 'VBC':
+                abbreviation = 'VAL'
+            case 'ZAL':
+                abbreviation = 'ŽAL'
+            case _:
+                abbreviation = club['tvCode']
+
+        name = club['name']
+        clubs.append((name, abbreviation))
+
+    return clubs
+
+
+def update_teams(conn: sqlite3.Connection, cur: sqlite3.Cursor):
+    clubs_api = get_teams()
+
+    conn = sqlite3.connect(DB_PATH)
+    cur = conn.cursor()
+
+    for club in clubs_api:
+        abbreviation = club[0]
+        name = club[1]
+
+        print("Syncing:", abbreviation, name)
+
+        cur.execute(
+            """
+            INSERT INTO Teams (team_name, abbreviation)
+            VALUES (?, ?)
+            ON CONFLICT(abbreviation) DO UPDATE
+                SET team_name = excluded.team_name;
+            """,
+            (name, abbreviation),
+        )
+
+    conn.commit()
+    conn.close()
+
+
+if __name__ == "__main__":
+    conn = sqlite3.connect(DB_PATH)
+    cur = conn.cursor()
+    update_teams(conn, cur)
+    print("Teams updated.")
